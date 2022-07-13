435240963ca1e8224cf78c2cae53899345712b98
juliamachin <juliaamachin@gmail.com>
2022-07-12 17:59:08 -0700

    title added to home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,14 @@
-import "./Home.css";
-
+import "./Home.scss";
+import TextAnimation from "../animations/TextAnimation";
 
 const Home = () => {
-  
   return (
-    <div className="home-page">
-    
+    <div className="home-page" id="box">
+      <p id="flashlight">
+        <span id="flash">Julia</span>
+        <span id="light">Machin</span>
+      </p>
+      <TextAnimation />
     </div>
   );
 };
