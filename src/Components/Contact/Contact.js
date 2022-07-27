c2a039acf79f9fa71c753c6c89b89066f6bda85f
juliamachin <juliaamachin@gmail.com>
2022-07-27 14:33:15 -0700

    added form to contact component

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,25 @@
-import './Contact.scss'
+import "./Contact.scss";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-    return (
-        <div>
-            <h3>Contact</h3>
-        </div>
-    )
-}
+  const form = useRef();
 
-export default Contact
+  return (
+    <div className="form-container">
+      <h2>Contact Me</h2>
+      <form>
+        <label>First Name:</label>
+        <input placeholder="first name" type="text" required />
+        <label>Last Name:</label>
+        <input placeholder="last name" type="text" />
+        <label>Email:</label>
+        <input placeholder="email" type="email" required />
+        <label>Subject:</label>
+        <textarea placeholder="subject" type="text" required></textarea>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
